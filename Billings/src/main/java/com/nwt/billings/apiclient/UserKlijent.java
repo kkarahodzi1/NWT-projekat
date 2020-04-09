14fcc0826d412d5350dbed6fa9237b9fb7e02d84
nvelic2 <nvelic2@etf.unsa.ba>
2020-04-09 21:31:54 +0200

    dodatne ispravke

--- Billings/src/main/java/com/nwt/billings/apiclient/UserKlijent.java
+++ Billings/src/main/java/com/nwt/billings/apiclient/UserKlijent.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "user-control-service")
+@FeignClient(name = "usercontrol-service")
 public interface UserKlijent {
 
-    @RequestMapping(value = "api/users/{id}", method = RequestMethod.POST, consumes = "application/json")
+    @RequestMapping(value = "api/users/{id}", method = RequestMethod.GET, consumes = "application/json")
     ResponseEntity<User> getUser(@PathVariable Long id);
 }
