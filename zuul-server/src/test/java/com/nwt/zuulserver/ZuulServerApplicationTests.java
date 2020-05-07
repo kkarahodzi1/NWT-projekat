aa4b4990bc6e75b12e82e99ecef9a37c1cc7fbaa
kkarahodzi1 <kkarahodzi1@etf.unsa.ba>
2020-05-07 15:30:35 +0200

    Dodan osnovni Zuul server

---
+++ zuul-server/src/test/java/com/nwt/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nwt.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
