111a8cb312ce22c0abf86d247bd0acb856a030ee
kkarahodzi1 <kkarahodzi1@etf.unsa.ba>
2020-05-27 20:57:47 +0200

    Dodane informacije o korisniku u token koji se vraća sa backenda

--- usercontrol/src/main/java/com/nwt/usercontrol/authServer/AuthorizationServerConfig.java
+++ usercontrol/src/main/java/com/nwt/usercontrol/authServer/AuthorizationServerConfig.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,10 +17,17 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -35,7 +44,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager);
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(
+                Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+        endpoints
+                .tokenStore(tokenStore())
+                .authenticationManager(authenticationManager)
+                .tokenEnhancer(tokenEnhancerChain);
     }
 
     @Override
@@ -60,5 +75,30 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .accessTokenValiditySeconds(30*60)
                 .resourceIds("resource")
                 .secret(passwordEncoder().encode("regular"));
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+
+    @Bean
+    public TokenEnhancer tokenEnhancer() {
+        return new CustomTokenEnhancer();
     }
 }
---
+++ usercontrol/src/main/java/com/nwt/usercontrol/authServer/CustomTokenEnhancer.java
@@ -0,0 +1,32 @@
+package com.nwt.usercontrol.authServer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.nwt.usercontrol.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+import org.springframework.security.core.userdetails.User;
+
+public class CustomTokenEnhancer implements TokenEnhancer {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken,
+                                     OAuth2Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        final Map<String, Object> additionalInfo = new HashMap<String, Object>();
+        additionalInfo.put("client", userRepository.findByMail(user.getUsername()));
+
+        ((DefaultOAuth2AccessToken) accessToken)
+                .setAdditionalInformation(additionalInfo);
+
+        return accessToken;
+    }
+}
