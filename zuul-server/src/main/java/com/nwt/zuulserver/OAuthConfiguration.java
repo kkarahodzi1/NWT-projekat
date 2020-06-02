dffd2e1012675f4a108c8fcfcbaa339ac19ddaf6
kkarahodzi1 <kkarahodzi1@etf.unsa.ba>
2020-06-02 13:34:44 +0200

    Ukinuta autorizacija OPTIONS zahtjeva

--- zuul-server/src/main/java/com/nwt/zuulserver/OAuthConfiguration.java
+++ zuul-server/src/main/java/com/nwt/zuulserver/OAuthConfiguration.java
@@ -2,6 +2,7 @@ package com.nwt.zuulserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -27,6 +28,7 @@ public class OAuthConfiguration extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .antMatchers("/user/api/secure/**").authenticated()
                 .antMatchers("/storage/api/**").authenticated()
                 .antMatchers("/billing/api/**").authenticated();
